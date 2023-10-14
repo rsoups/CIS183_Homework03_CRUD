8591ac46b204241d6a399ec10b97ba9db184780c
rsoups <rethun@my.monroeccc.edu>
2023-10-14 12:28:55 -0400

    Main Page
    -main page gui added

--- app/src/main/java/com/example/cis183_homework03_crudapp/MainActivity.java
+++ app/src/main/java/com/example/cis183_homework03_crudapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.cis183_homework03_crudapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_j_addEmployee;
+    ListView lv_j_listOfEmployees;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_j_addEmployee = findViewById(R.id.btn_v_addEmployee);
+        lv_j_listOfEmployees = findViewById(R.id.lv_v_listOfEmployees);
     }
 }
