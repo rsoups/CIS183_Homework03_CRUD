8de942745c62139a8ad0572bf8ecfbd84cc2dad1
rsoups <rethun@my.monroeccc.edu>
2023-10-14 06:59:51 -0400

    Initial Project Commit
    project created on android studio

---
+++ app/src/main/java/com/example/cis183_homework03_crudapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cis183_homework03_crudapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
